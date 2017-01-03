dc3adb281a3af9d5182a0bff27c0ecdf85cb10cf
Fletcher91 <thom@argu.co>
2017-01-03 17:38:22 +0100

    [FIX] Automatically include the set context properties as childContextType

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,4 +1,5 @@
 import LinkedRenderStore from 'link-lib';
+import { PropTypes } from 'react';
 import { applyMiddleware, createStore } from 'redux';
 
 import { linkMiddleware, linkReducer } from '../src/index';
@@ -17,13 +18,16 @@ const contextDefaults = {
 function generateContext(properties = {}) {
   const keys = Object.keys(properties);
   const c = {
+    childContextTypes: {},
     context: {},
   };
   keys.forEach((key) => {
     if (properties[key] === true) {
       c.context[key] = contextDefaults[key];
+      c.childContextTypes[key] = PropTypes.object;
     } else if (properties[key] !== undefined) {
       c.context[key] = properties[key];
+      c.childContextTypes[key] = PropTypes.object;
     }
   });
   return c;
