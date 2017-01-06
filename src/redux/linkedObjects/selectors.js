e624a80018a9dc395b62c25d05b869cd87154b48
Fletcher91 <thom@argu.co>
2017-01-06 15:01:22 +0100

    [IMP] Also return data from objects marked with `owl:sameAs`.

--- src/redux/linkedObjects/selectors.js
+++ src/redux/linkedObjects/selectors.js
@@ -1,7 +1,17 @@
 export const selectLinkedObjectById = (state, iri) =>
   state.getIn(['linkedObjects', 'items', iri]);
 
-export const selectLinkedObject = (state, props) =>
-  selectLinkedObjectById(state, props.object);
+export const selectLinkedObject = (state, props) => {
+  const obj = selectLinkedObjectById(state, props.object);
+  if (obj === undefined) {
+    return obj;
+  }
+  const same = obj.getIn(['http://www.w3.org/2002/07/owl#sameAs', '@id']);
+  if (same === undefined) {
+    return obj;
+  }
+  return selectLinkedObjectById(state, same)
+    .merge(selectLinkedObjectById(state, props.object));
+};
 
 export default selectLinkedObject;
