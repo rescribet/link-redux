858b0c9e25bdb7c44452fd31d8b839ad2917c66d
Fletcher91 <thom@argu.co>
2017-01-17 14:03:04 +0100

    [IMP] Replaced `redux-actions` reducer with a POJO
    This saves somewhere around 20K for projects not using that dependency

--- src/redux/linkedObjects/reducer.js
+++ src/redux/linkedObjects/reducer.js
@@ -1,4 +1,3 @@
-import { handleActions } from 'redux-actions';
 import { Map } from 'immutable';
 
 import { FETCH_LINKED_OBJECT } from './actions';
@@ -7,21 +6,26 @@ const initialState = new Map({
   items: new Map(),
 });
 
-export default handleActions({
-  [FETCH_LINKED_OBJECT]: (state, action) => {
-    const { error, payload } = action;
-    if (error !== true) {
-      const id = payload.record['@id'] || payload.href;
-      return state.setIn(
-        ['items', id],
-        (state.getIn(['items', id]) || new Map())
-          .mergeDeep(payload.record)
-          .merge({
-            href_url: payload.href,
-            loading: payload.loading,
-          }),
-      );
-    }
-    return state;
-  },
-}, initialState);
+function linkReducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_LINKED_OBJECT:
+      const { error, payload } = action;
+      if (error !== true) {
+        const id = payload.record['@id'] || payload.href;
+        return state.setIn(
+          ['items', id],
+          (state.getIn(['items', id]) || new Map())
+            .mergeDeep(payload.record)
+            .merge({
+              href_url: payload.href,
+              loading: payload.loading,
+            }),
+        );
+      }
+      return state;
+    default:
+      return state;
+  }
+}
+
+export default linkReducer;
