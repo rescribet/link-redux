3a2d45cd50f75a1344111d0cac226b0c666101a7
Fletcher91 <thom@argu.co>
2017-08-14 10:59:41 +0200

    [DEP] Upgrade react, webpack and sinon

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,30 @@ var libraryName = 'link-redux';
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: './dist',
+    path: path.resolve('./dist'),
     filename: libraryName + '.js',
     library: libraryName,
     libraryTarget: 'commonjs2'
   },
   externals: {
+    // Runtime
     redux: 'redux',
     react: 'react',
-    immutable: 'immutable',
+    'react-redux': 'react-redux',
     rdflib: 'rdflib',
-    'link-lib': 'link-lib'
+    'link-lib': 'link-lib',
+
+    // Testtime
+    chai: 'chai',
+    enzyme: 'enzyme',
+    'chai-enzyme': 'chai-enzyme',
+    'sinon-chai': 'sinon-chai',
+    immutable: 'immutable',
+    'react/addons': true,
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true,
+    'react-addons-test-utils': 'react-dom',
+    'redux-immutable': 'redux-immutable'
   },
   module: {
     rules: [
