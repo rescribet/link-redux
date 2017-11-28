f9a273a0adef8023b21688907bdcb104d4a594dc
Fletcher91 <thom@argu.co>
2017-11-28 18:02:29 +0100

    [IMP] Expose getLinkedObjectClass for dev aid

--- src/react/components/index.js
+++ src/react/components/index.js
@@ -5,5 +5,8 @@ export {
   getLinkedObjectProperty,
   getLinkedObjectPropertyRaw,
 } from './PropertyBase';
-export { default as Property } from './Property';
+export {
+  default as Property,
+  getLinkedObjectClass,
+} from './Property';
 export { default as RenderStoreProvider } from './RenderStoreProvider';
