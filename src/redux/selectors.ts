831ded910938b3b5236d21096e211ed3e00d2b06
Fletcher91 <thom@argu.co>
2018-09-01 12:23:43 +0200

    [IMP] Include URL in object expiration
    Resources with fragments should also update when their parent resource
    changes, since they can't be fetched individually.

--- src/redux/selectors.ts
+++ src/redux/selectors.ts
@@ -1,12 +1,24 @@
+import { namedNodeByIRI } from "link-lib";
 import { BlankNode, NamedNode } from "rdflib";
 
 import { LinkStateTree, SubjectType } from "../types";
 
 export const linkedObjectVersionByIRI = (state: LinkStateTree, iri: SubjectType): string => {
+    const irl = iri.termType === "NamedNode" && namedNodeByIRI(iri.value.split("#").shift()!);
+    let tree;
     if ("linkedObjects" in state) {
-        return state.linkedObjects[iri.value];
+        tree = state.linkedObjects;
+    } else {
+        tree = state.get("linkedObjects");
     }
-    const s = state.get("linkedObjects");
 
-    return s ? s[iri.value] : "";
+    if (!tree || !iri) {
+        return "";
+    }
+
+    if (irl && irl !== iri) {
+        return tree[iri.value] + tree[irl.value];
+    }
+
+    return tree[iri.value];
 };
