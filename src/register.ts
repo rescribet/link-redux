ba265b89d2b11b3eb56b0bacd1904aed56d8f316
Fletcher91 <thom@argu.co>
2018-09-12 17:24:12 +0200

    [FIX] Wrap `link` hoc last
    So other higher order components can consume/transform server data.

--- src/register.ts
+++ src/register.ts
@@ -10,14 +10,14 @@ export type higherOrderWrapper<TNeedsProps> = InferableComponentEnhancerWithProp
 export function register<P>(comp: RegistrableComponent<P>, ...hocs: Array<higherOrderWrapper<P>>):
     Array<ComponentRegistration<ComponentType<P>>> {
 
-    const dataBoundComp = comp.mapDataToProps ? link(comp.mapDataToProps, comp.linkOpts)(comp) : comp;
-
     const reducer = (prev: ComponentClass<P>, cur: higherOrderWrapper<P>) => cur(prev);
     // @ts-ignore
-    const wrappedComp = (comp.hocs || hocs).reduce(reducer, dataBoundComp);
+    const wrappedComp = (comp.hocs || hocs).reduce(reducer, comp);
+
+    const dataBoundComp = comp.mapDataToProps ? link(comp.mapDataToProps, comp.linkOpts)(wrappedComp) : wrappedComp;
 
     return LinkedRenderStore.registerRenderer(
-        wrappedComp,
+        dataBoundComp,
         comp.type,
         comp.property,
         comp.topology,
