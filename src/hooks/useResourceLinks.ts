d6fb26688b5953073e17bd8a0067a3357556a1ce
Fletcher91 <thom@argu.co>
2020-04-01 10:54:38 +0200

    Enable data fetching for useLink hooks

--- src/hooks/useResourceLinks.ts
+++ src/hooks/useResourceLinks.ts
@@ -3,6 +3,7 @@ import { normalizeType } from "link-lib";
 import React from "react";
 
 import { dataPropsToPropMap } from "../hocs/link/dataPropsToPropMap";
+import { globalLinkOptsDefaults } from "../hocs/link/globalLinkOptsDefaults";
 import ll from "../ontology/ll";
 import { LinkOpts, MapDataToPropsParam } from "../types";
 import { useDataInvalidation } from "./useDataInvalidation";
@@ -31,16 +32,25 @@ export function useResourceLinks(
 
       for (let i = 0; i < len; i++) {
         const subject = dataSubjects[i];
+
+        if ((opts.fetch ?? globalLinkOptsDefaults.fetch)
+            && isNamedNode(subject)
+            && lrs.shouldLoadResource(subject)) {
+          lrs.queueEntity(subject);
+        }
+
         const subjectData = lrs.tryEntity(subject);
         const subjProps = [
           // Ensure the first item's subject is always the data subject
           rdfFactory.quad(subject, ll.dataSubject, ll.nop),
         ];
+
         for (let j = 0; j < subjectData.length; j++) {
           if (requestedProperties.includes(rdfFactory.id(subjectData[j].predicate))) {
             subjProps.push(subjectData[j]);
           }
         }
+
         sets.push(subjProps);
       }
 
