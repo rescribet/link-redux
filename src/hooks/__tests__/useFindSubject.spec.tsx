393756261fef5a1af211076f925cc194b8da31ad
Thom <thom@argu.co>
2022-08-22 11:40:53 +0200

    test: add test cases for useFindSubject

--- src/hooks/__tests__/useFindSubject.spec.tsx
+++ src/hooks/__tests__/useFindSubject.spec.tsx
@@ -5,9 +5,54 @@ import { render } from "@testing-library/react";
 import React from "react";
 
 import * as ctx from "../../__tests__/helpers/fixtures";
+import example from "../../ontology/example";
 import { useFindSubject } from "../useFindSubject";
 
 describe("useFindSubject", () => {
+  it("handles empty subject", () => {
+    const opts = ctx.empty();
+
+    const UpdateComp = () => {
+      const [subject] = useFindSubject(
+        [schema.reviews, rdfx.rest, rdfx.rest, rdfx.first],
+        rdfFactory.literal("r2"),
+        undefined,
+      );
+
+      return (
+        <div data-testid="id">
+          {subject}
+        </div>
+      );
+    };
+
+    const { getByTestId } = render(opts.wrapComponent(<UpdateComp />));
+
+    expect(getByTestId("id")).toBeEmptyDOMElement();
+  });
+
+  it("handles array subjects", () => {
+    const opts = ctx.fullCW();
+
+    const UpdateComp = () => {
+      const [subject] = useFindSubject(
+        [schema.reviews, rdfx.rest, rdfx.rest, rdfx.first],
+        rdfFactory.literal("r2"),
+        [example.ns("3")],
+      );
+
+      return (
+        <div data-testid="id">
+          {subject[0]?.value}
+        </div>
+      );
+    };
+
+    const { getByTestId } = render(opts.wrapComponent(<UpdateComp />));
+
+    expect(getByTestId("id")).toHaveTextContent("reviews2");
+  });
+
   it("finds a subject", () => {
     const opts = ctx.fullCW();
 
